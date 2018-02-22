ca7753cd1ebd5ae449801db5c54a9d77a462c146
G M <814066152@wwcc.edu>
2018-02-22 09:56:08 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,13 @@ RSpec.configure do |config|
   config.order = 'default'
 end
 
-def get_variable_from_file(file, greeting)
+def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
 
   begin
-    return file_scope.local_variable_get(greeting)
+    return file_scope.local_variable_get(variable)
   rescue NameError
-    raise NameError, "local variable `#{greeting}` not defined in #{file}."
+    raise NameError, "local variable `#{variable}` not defined in #{file}."
   end
 end
